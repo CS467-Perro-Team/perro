d695de4e24f12689a8ba8972739e1dc36a2a57ce
Robert Rose <rose@perrito2.attlocal.net>
2016-03-13 00:11:49 -0800

    The start of tracking

--- perro.js
+++ perro.js
@@ -43,7 +43,141 @@ var getNode = function(data, id) {
   return undefined;
 };
 
-var MyApp = angular.module('MyApp', ['ui.router', 'ui.tree']);
+var getMinMaxDates = function(node) {
+  var min_date = new Date("2100/01/01");
+  var max_date = new Date("1970/01/01");
+  for (var i = 0; i < node.length; i++) {
+
+    // Get minimum date for this node.
+    if (node[i].data) {
+      for (var j = 0; j < node[i].data.length; j++) {
+        var data_date = new Date(node[i].data[j].date);
+        if (data_date < min_date) {
+          min_date = data_date;
+        }
+        if (data_date > max_date) {
+          max_date = data_date;
+        }
+      }
+    }
+
+    // Traverse children.
+    if (node[i].nodes) {
+      var child_dates = getMinMaxDates(node[i].nodes);
+      if (child_dates.min < min_date) {
+        min_date = child_dates.min;
+      }
+      if (child_dates.max > max_date) {
+        max_date = child_dates.max;
+      }
+    }
+  }
+
+  return {
+    min: min_date,
+    max: max_date
+  };
+};
+
+var getDateRange = function(min_date, max_date) {
+  // Find the start and end in 1w offsets.
+  var start = min_date;
+  start.setDate(min_date.getDate() - min_date.getDay());
+  var end = max_date;
+  end.setDate(end.getDate() + (7 - end.getDay()));
+
+  // Generate the date range in 1w increments.
+  var date_range = [];
+  while (start <= end) {
+    date_range.push(new Date(start));
+    start.setDate(start.getDate() + 7);
+  }
+
+  return date_range;
+};
+
+var visitNodes = function(node, start, end, cb) {
+  for (var i = 0; i < node.length; i++) {
+
+    // Traverse this node's data.
+    if (node[i].data) {
+      for (var j = 0; j < node[i].data.length; j++) {
+        var data_date = new Date(node[i].data[j].date);
+        if (data_date >= start && data_date < end) {
+          cb(node[i].data[j]);
+        }
+      }
+    }
+
+    // Traverse this node's children.
+    if (node[i].nodes) {
+      visitNodes(node[i].nodes, start, end, cb);
+    }
+  }
+};
+
+var getTrackingSeries = function(node) {
+  var min_max_dates = getMinMaxDates(node);
+  if (min_max_dates.max < new Date()) {
+    min_max_dates.max = new Date();
+  }
+  var date_range = getDateRange(min_max_dates.min, min_max_dates.max);
+
+  var progress = Array(date_range.length);
+  var remaining = Array(date_range.length);
+  var total = Array(date_range.length);
+
+  // Walk the node in time order.
+  var t;
+  for (t = 0; t < date_range.length - 1; t++) {
+    var start = date_range[t];
+    var end = date_range[t+1];
+
+    // progress and remaining carry forward from previous interval.
+    if (t === 0) {
+      progress[t] = 0;
+      remaining[t] = 0;
+    } else {
+      progress[t] = progress[t-1];
+      remaining[t] = remaining[t-1];
+    }
+
+    visitNodes(node, start, end, function(data) {
+      progress[t] += data.progress;
+      remaining[t] = data.remaining;
+    });
+
+    total[t] = progress[t] + remaining[t];
+  }
+
+  // zip the result into a timeseries.
+  var progress_series = [];
+  var remaining_series = [];
+  var total_series = [];
+
+  for (t = 0; t < date_range.length - 1; t++) {
+    progress_series.push([date_range[t].getTime(), progress[t]]);
+    remaining_series.push([date_range[t].getTime(), remaining[t]]);
+    total_series.push([date_range[t].getTime(), total[t]]);
+  }
+
+  return [
+    {
+      data: progress_series,
+      name: 'Progress'
+    },
+    {
+      data: remaining_series,
+      name: 'Remaining'
+    },
+    {
+      data: total_series,
+      name: 'Total'
+    }
+  ];
+};
+
+var MyApp = angular.module('MyApp', ['ui.router', 'ui.tree', 'highcharts-ng']);
 
 MyApp.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/");
@@ -64,6 +198,15 @@ MyApp.config(function($stateProvider, $urlRouterProvider) {
       'area': {
         templateUrl: 'insert.html',
         controller: 'InsertController'
+      }
+    }
+  })
+  .state('project.tracking', {
+    url: '/tracking',
+    views: {
+      'area': {
+        templateUrl: 'tracking.html',
+        controller: 'TrackingController'
       }
     }
   });
@@ -151,5 +294,39 @@ MyApp.controller('InsertController', function($scope, $state) {
         return;
       }
     }
+  };
+});
+
+MyApp.controller('TrackingController', function($scope, $state) {
+  $scope.dates = getMinMaxDates([$scope.$parent.node]);
+  /*
+  $scope.series = [
+    {
+      data: [
+        [(new Date("2010/01/01")).getTime(), 1],
+        [(new Date("2010/01/02")).getTime(), 2],
+        [(new Date("2010/01/03")).getTime(), 3],
+      ],
+      name: 'foo'
+    }
+  ];*/
+
+  $scope.series = getTrackingSeries([$scope.$parent.node]);
+
+  $scope.chart_config = {
+    options: {
+      //This is the Main Highcharts chart config. Any Highchart options are valid here.
+      //will be overriden by values specified below.
+      chart: {
+        
+      },
+      xAxis: {
+        type: 'datetime'
+      },
+    },
+    title: {
+      text: $scope.$parent.node.title
+    },
+    series: $scope.series,
   };
 });
